e17e14be6005ac890f3d3edc71430b2b4318e5d8
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-01-26 12:20:32 -0500

    Clarifies reason for error

--- error.go
+++ error.go
@@ -5,7 +5,7 @@ import (
 	"net/url"
 )
 
-// OAuth2 errors in accordance with:
+// Implements OAuth2 errors in accordance with:
 // http://tools.ietf.org/html/rfc6749#section-4.1.2.1
 // http://tools.ietf.org/html/rfc6749#section-4.2.2.1
 // http://tools.ietf.org/html/rfc6749#section-5.2
@@ -30,7 +30,7 @@ var (
 
 	ErrRedirectURLInvalid = AuthzError{
 		Code: "access_denied",
-		Desc: "3rd-party client app provided an invalid redirect_uri. It does not comply with http://tools.ietf.org/html/rfc3986#section-4.3",
+		Desc: "3rd-party client app provided an invalid redirect_uri. It does not comply with http://tools.ietf.org/html/rfc3986#section-4.3 or does not use HTTPS.",
 	}
 
 	ErrClientIDMissing = AuthzError{
