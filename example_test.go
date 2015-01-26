2ec4b55d0e9a55dc74460fa360cb7f9213a0b8d6
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-01-26 15:39:37 -0500

    Updates example to reflect timeout configurations

--- example_test.go
+++ example_test.go
@@ -3,6 +3,7 @@ package oauth2
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/hooklift/oauth2"
 )
@@ -18,11 +19,15 @@ func main() {
 	authzForm := []byte("<html><h1>App ABC wants to access XYZ...</h1></html>")
 	reqHandler := oauth2.Handler(
 		mux,
-		// Sets request context, this is use to check if there is a valid session or not
-		// before displaying the authorization form to the resource owner.
 		oauth2.SetTokenEndpoint("/oauth2/tokens"),
 		oauth2.SetAuthzEndpoint("/oauth2/authzs"),
 		oauth2.SetRevokeEndpoint("/oauth2/revoke"),
+		// When setting token expiration times, the lower they are the more
+		// frequent your server is going to receive refresh tokens requests.
+		// When the opposite is done, you will be widening the time window for
+		// successful attacks. A reasonable value is 5 or 10 minutes.
+		oauth2.SetTokenExpiration(time.Duration(5)*time.Minute),
+		oauth2.SetAuthzExpiration(time.Duration(1)*time.Minute),
 		// Disables Strict Transport Security for development purposes
 		oauth2.SetSTSMaxAge(0),
 		// Sets authorization HTML form
