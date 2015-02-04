0747f748137444d002a4df5e1bb2bef36a1dabad
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-02-04 14:50:06 -0500

    Attempts to fix Travis integration

--- examples/example.go
+++ examples/example.go
@@ -2,7 +2,7 @@
 // License, version 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-package examples
+package examples_test
 
 import (
 	"log"
